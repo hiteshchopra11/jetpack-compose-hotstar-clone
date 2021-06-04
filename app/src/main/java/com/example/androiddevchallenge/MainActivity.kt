045da5aeec98b98911751d7100644c0f290cb95b
Hitesh Chopra <hitesh.chopra@mutualmobile.com>
2021-06-05 00:44:31 +0530

    Added Resources

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -6,45 +6,37 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.material.Button
-import androidx.compose.material.DrawerValue
-import androidx.compose.material.ModalDrawer
-import androidx.compose.material.Text
-import androidx.compose.material.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androiddevchallenge.ui.Drawer
 import com.example.androiddevchallenge.ui.theme.MyTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val context: Context = applicationContext
-        setContent {
-            MyTheme {
-                MyScreenContent(context)
-            }
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val context: Context = applicationContext
+    setContent {
+      MyTheme {
+        MyScreenContent(context)
+      }
     }
+  }
 }
 
 @Composable
 fun MyScreenContent(context: Context) {
-    Column(modifier = Modifier.fillMaxHeight()) {
-        Drawer()
-    }
+  Column(modifier = Modifier.fillMaxHeight()) {
+    Drawer()
+  }
 }
-
 
 @Preview
 @Composable
 fun Show() {
-    MyTheme {
-        Column(modifier = Modifier.fillMaxHeight()) {
-            Drawer()
-        }
+  MyTheme {
+    Column(modifier = Modifier.fillMaxHeight()) {
+      Drawer()
     }
+  }
 }
