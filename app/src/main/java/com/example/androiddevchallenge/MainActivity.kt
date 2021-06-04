8e7fe22702bac8839cdc2d22259a264ac7c20497
Hitesh Chopra <hitesh.chopra@mutualmobile.com>
2021-06-04 21:49:50 +0530

    Compose setup

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -1,61 +1,73 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.androiddevchallenge
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.material.Button
+import androidx.compose.material.DrawerValue
+import androidx.compose.material.ModalDrawer
 import androidx.compose.material.Text
+import androidx.compose.material.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MyTheme {
-                MyApp()
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val context: Context = applicationContext
+    setContent {
+      MyTheme {
+        MyScreenContent(context)
+      }
+    }
+  }
+}
+
+@Composable
+fun MyScreenContent(context: Context) {
+  Column(modifier = Modifier.fillMaxHeight()) {
+    ModalDrawerSample()
+  }
+}
+
+@Composable
+fun ModalDrawerSample() {
+  val drawerState = rememberDrawerState(DrawerValue.Closed)
+  val scope = rememberCoroutineScope()
+
+  ModalDrawer(
+    drawerState = drawerState,
+    drawerContent = {
+      Column {
+        Text("Text in Drawer")
+        Button(onClick = {
+          scope.launch {
+            drawerState.close()
+          }
+        }) {
+          Text("Close Drawer")
         }
-    }
-}
+      }
+    },
+    content = {
+      Column {
+        Text("Text in Bodycontext")
+        Button(onClick = {
 
-// Start building your app here!
-@Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
-    }
-}
+          scope.launch {
+            drawerState.open()
+          }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
+        }) {
+          Text("Click to open")
+        }
+      }
     }
-}
-
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
-    }
+  )
 }
