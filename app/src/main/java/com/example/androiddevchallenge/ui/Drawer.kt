1b8b915edadb42e3b68e76d788a462dc1b141259
hiteshchopra11 <hiteshchopra11@gmail.com>
2021-06-05 03:04:53 +0530

    Drawer Layout

--- app/src/main/java/com/example/androiddevchallenge/ui/Drawer.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/Drawer.kt
@@ -1,47 +1,135 @@
 package com.example.androiddevchallenge.ui
 
-import android.view.View
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.drawerBackground
+import com.example.androiddevchallenge.ui.theme.drawerItemSubtitle
+import com.example.androiddevchallenge.ui.theme.drawerItemTitle
 
-sealed class DrawerScreens(val title: String) {
-    object Home : DrawerScreens("Home")
-    object Account : DrawerScreens("Account")
-    object Help : DrawerScreens( "Help")
+sealed class DrawerScreens(val title: String, val icon: Int, val subtitle: String? = null) {
+    object Downloads : DrawerScreens("Downloads", R.drawable.ic_download, "Watch videos offline")
+    object Watchlist : DrawerScreens("Watchlist", R.drawable.ic_watchlist, "Save to watch later")
+    object Prizes : DrawerScreens("Prizes", R.drawable.ic_prizes, "Prizes you have won")
+    object Movies : DrawerScreens("Movies", R.drawable.ic_movies)
+    object Premium : DrawerScreens("Premium", R.drawable.ic_premium)
+    object Trending : DrawerScreens("Trending", R.drawable.ic_trending)
+    object Channels : DrawerScreens("Channels", R.drawable.ic_channels)
+    object Languages : DrawerScreens("Languages", R.drawable.ic_langauges)
+    object Genres : DrawerScreens("Genres", R.drawable.ic_genres)
 }
 
 private val screens = listOf(
-    DrawerScreens.Home,
-    DrawerScreens.Account,
-    DrawerScreens.Help
+    DrawerScreens.Downloads,
+    DrawerScreens.Watchlist,
+    DrawerScreens.Prizes,
+    DrawerScreens.Movies,
+    DrawerScreens.Premium,
+    DrawerScreens.Trending,
+    DrawerScreens.Channels,
+    DrawerScreens.Languages,
+    DrawerScreens.Genres,
 )
 
 @Composable
 fun Drawer(
-    modifier: Modifier = Modifier) {
-    Column(
-        modifier
-            .fillMaxSize()
-            .padding(start = 24.dp, top = 48.dp)
-    ) {
-        Text("Hitesh Chopra")
-        Text("7009959730")
-        screens.forEach { screen ->
-            Spacer(Modifier.height(24.dp))
+    modifier: Modifier = Modifier
+) {
+    Row {
+        Column(
+            modifier
+                .background(drawerBackground)
+                .fillMaxSize()
+                .padding(top = 30.dp)
+        ) {
             Text(
-                text = screen.title,
-                style = MaterialTheme.typography.h4
+                "Hitesh Chopra",
+                color = drawerItemTitle,
+                fontSize = 26.sp,
+                modifier = Modifier.padding(start = 24.dp)
             )
+            Spacer(modifier = Modifier.padding(4.dp))
+            Text(
+                "+91 7009959730",
+                color = drawerItemSubtitle,
+                fontSize = 20.sp,
+                modifier = Modifier.padding(start = 24.dp)
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            Box(
+                modifier = Modifier
+                    .height(50.dp)
+                    .fillMaxWidth()
+                    .background(Color(0xFF1b1f20))
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painterResource(id = R.drawable.ic_kids),
+                        contentDescription = "",
+                        tint = Color(0xFFF1bf5e),
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(16.dp,16.dp,6.dp,16.dp)
+                    )
+                    Text(
+                        "Safe",
+                        color = Color(0xFFF1bf5e),
+                        fontSize = 18.sp,
+                        fontFamily = FontFamily(Font(R.font.istok_bold))
+                    )
+                }
+            }
+            screens.forEach { screen ->
+                Spacer(modifier = Modifier.height(24.dp))
+                Row(modifier = Modifier.padding(start = 16.dp)) {
+                    if (screen.subtitle == null) {
+                        Icon(
+                            painterResource(id = screen.icon),
+                            contentDescription = null,
+                            tint = Color(0xFF8F9999),
+                        )
+                    } else {
+                        Icon(
+                            painterResource(id = screen.icon),
+                            contentDescription = null,
+                            tint = Color(0xFF8F9999),
+                            modifier = Modifier
+                                .padding(0.dp, 12.dp, 0.dp, 0.dp)
+                        )
+                    }
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    Column {
+                        Text(
+                            text = screen.title,
+                            color = drawerItemTitle,
+                            fontSize = 20.sp
+                        )
+                        screen.subtitle?.let {
+                            Text(
+                                text = it,
+                                color = drawerItemSubtitle,
+                                fontSize = 16.sp
+                            )
+                        }
+                    }
+                }
+            }
         }
+        Spacer(modifier = Modifier.padding(8.dp))
     }
 }
 
